d0539d74dddac267ca4c6d857a4343a43d761b07
afifhusain <afifhusain2014@gmail.com>
2020-02-20 18:02:09 +0700

    terbaru

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -10,6 +10,12 @@
 
 <div class="row">
 	<div class="col-lg">
+		
+		<?php if (validation_errors()) : ?>
+			<div class="alert alert-danger" role="alert">
+			<?= validation_errors(); ?>
+			</div>
+		<?php endif; ?>
 
 		<?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
 
@@ -17,7 +23,7 @@
 
 
 
-		<a href="" class="btn btn-info" data-toggle="modal" data-target="#Submenu">Tambahkan Sub Menu Baru</a>
+		<a href="" class="btn btn-info" data-toggle="modal" data-target="#submenu">Tambahkan Sub Menu Baru</a>
 			
 
 		<table class="table table-striped">
@@ -42,7 +48,7 @@
 		    <tr>
 		      <th scope="row"><?= $a++; ?></th>
 		      <td><?= $sub['title']; ?></td>
-		      <td><?= $sub['menu_id']; ?></td>
+		      <td><?= $sub['menu']; ?></td>
 		      <td><?= $sub['url']; ?></td>
 		      <td><?= $sub['icon']; ?></td>
 		      <td><?= $sub['is_active']; ?></td>
@@ -75,27 +81,53 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="submenu" tabindex="-1" role="dialog" aria-labelledby="submenu" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Halaman Tambah Menu</h5>
+        <h5 class="modal-title" id="submenu">Halaman Tambah Menu</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
 
-    <form action="<?= base_url('/menu') ?>" method="post">
+    <form action="<?= base_url('/menu/submenu') ?>" method="post">
     <div class="modal-body">
 
 
    	<div class="form-group">
-	    <input type="text" class="form-control" id="menu" placeholder="namamenu" name="menu">
+	    <input type="text" class="form-control" id="title" placeholder="Submenu title" name="title">
+  	</div>
+  	<div class="form-group">
+
+		<select name="menu_id" id="menu-id" class="form-control">
+			<option value="">select menu</option>
+		<?php foreach ($menu as $m) : ?>
+			<option value="<?= $m['id']; ?>"> <?= $m['menu']; ?></option>
+		<?php endforeach; ?>
+		</select>
+
+  	</div>
+
+  	<div class="form-group">
+	    <input type="text" class="form-control" id="url" placeholder="Submenu url" name="url">
   	</div>
         	
+	<div class="form-group">
+	    <input type="text" class="form-control" id="icon" placeholder="Submenu icon" name="icon">
+  	</div>
 
 
-
+  	<div class="form-group">
+  		<div class="form-check">
+  			<div class="custom-control custom-checkbox">
+			  <input type="checkbox" class="custom-control-input" name="is_active" id="is_active" value="1" checked>
+			  <label class="custom-control-label" for="customCheck1" for="is_active">Aktif ?</label>
+			</div>
+  		</div>
+  	</div>
+	
+	
        
       </div>
       <div class="modal-footer">
